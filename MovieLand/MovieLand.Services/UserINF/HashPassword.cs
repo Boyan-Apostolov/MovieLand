14f02f9de6f781a9f6d2349ce7577d2317859f46
LuisSlv <luis.slavov@abv.bg>
2022-02-16 22:31:17 +0200

    Added Password Hashing

---
+++ MovieLand/MovieLand.Services/UserINF/HashPassword.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace MovieLand.Services.UserINF
+{
+    class HashPassword
+    {
+        private string username;
+        public string Username
+        {
+            get { return username; }
+
+            set
+            {
+                if (value != "'" || value != "/" || value != "*" || value == "!")
+                {
+                    username = value;
+                }
+                else
+                {
+                    string ToTyepe = "//INVALID USERNAME!//";
+                    Console.WriteLine(String.Format("{0,"
+                        + ((Console.WindowWidth / 2)
+                        + (ToTyepe.Length / 2))
+                        + "}", ToTyepe));
+                }
+            }
+        }
+
+        private string hPassword;
+        public string HPassword
+        {
+            get { return hPassword; }
+
+            set
+            {
+                if (value != "'" || value != "/" || value != "*" || value == "!")
+                {
+                    hPassword = value;
+                }
+                else
+                {
+                    string ToType = "//INVALID PASSWORD!//";
+                    Console.WriteLine(String.Format("{0,"
+                        + ((Console.WindowWidth / 2)
+                        + (ToType.Length / 2))
+                        + "}", ToType));
+
+                    ConvertPassword();
+                }
+            }
+        }
+
+
+        public static string ConvertPassword()
+        {
+            string Password;
+            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+
+            Console.Write("Password:");
+            Password = Console.ReadLine();
+
+            byte[] password_bytes = Encoding.ASCII.GetBytes(Password);
+            byte[] encrypted_bytes = sha1.ComputeHash(password_bytes);
+
+            return Convert.ToBase64String(encrypted_bytes);
+            //ADD TO DB
+        }
+    }
+}
+
