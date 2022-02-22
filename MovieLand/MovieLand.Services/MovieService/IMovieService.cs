6bfd7b55b2475d03cf1720a824c186f8e4435d00
PetyaPet <99098580+PetyaPet@users.noreply.github.com>
2022-02-22 19:42:20 +0200

    movieServices

---
+++ MovieLand/MovieLand.Services/MovieService/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieLand.Data.Models;
+
+namespace MovieLand.Services.MovieService
+{
+    public interface IMovieService
+    {
+        public void CreateMovie(string title, string plot, string producer, string genre);
+
+        public Movie? GetMovie(int id);
+
+        public void DeleteMovie(int id);
+    }
+}
---
+++ MovieLand/MovieLand.Services/MovieService/MovieService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieLand.Data.Models;
+using MovieLand.Data;
+using System.Linq;
+
+namespace MovieLand.Services.MovieService
+{
+    public class MovieService : IMovieService
+    {
+        private readonly MovieLandDbContext dbContext;
+
+        public MovieService(MovieLandDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void CreateMovie( string title, string plot, string producer, string genre)
+        {
+            var movie = new Movie()
+            {
+                Title = title,
+                Plot= plot,
+                Producer=producer,
+                Genre=genre
+            };
+            this.dbContext.Movies.Remove(movie);
+            this.dbContext.SaveChanges();
+        }
+
+        public Movie? GetMovie(int id)
+        {
+            var movie = new Movie()
+            {
+                Id = id
+            };
+
+            if (this.dbContext.Movies.Any(m => m.Id == id))
+            {
+                return movie;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void DeleteMovie(int id)
+        {
+            var movie = new Movie()
+            {
+                Id=id
+            };
+            if (this.dbContext.Movies.Any(m => m.Id == id))
+            {
+                this.dbContext.Movies.Remove(movie);
+                this.dbContext.SaveChanges();
+            }
+            
+        }
+    }
+}
